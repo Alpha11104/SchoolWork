2df0164b88f86f05c9dc1ef183606a93413cefab
Arthur de Villiers <arthurdevilliers@gmail.com>
2016-02-20 12:10:49 +0200

    Added some test comments

--- Guessing game_1/src/Guessing.java
+++ Guessing game_1/src/Guessing.java
@@ -1,29 +1,30 @@
 import javax.swing.JOptionPane;
 
+/* Testing comments */
 public class Guessing {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		boolean run = true; // Boolean loop control
-		while (run) {
+        boolean run = true; // Boolean loop control
+        while (run) {
 
-			int raai = 11;
-			String getal = JOptionPane
-					.showInputDialog("Sleutel getal tussen 1 en 40");
-			int get = Integer.parseInt(getal);
+            int raai = 11;
+            String getal = JOptionPane
+                    .showInputDialog("Sleutel getal tussen 1 en 40");
+            int get = Integer.parseInt(getal);
 
-			if (get == raai)
-				System.out.println(get + " Is die korrekte getal");
-			else if (get <= 11)
-				System.out.println("+");
-			else if (get >= 11)
-				System.out.println("-");
+            if (get == raai)
+                System.out.println(get + " Is die korrekte getal");
+            else if (get <= 11)
+                System.out.println("+");
+            else if (get >= 11)
+                System.out.println("-");
 
-			if (get == raai)
-				run = false; // loop closing statement
+            if (get == raai)
+                run = false; // loop closing statement
 
-		}
+        }
 
-	}
+    }
 
 }
